8a9e806f61c5159d665c1ddf943bcc6fe1b52aa3
Leonardo Guarnieri de Bastiani <leogbastiani@gmail.com>
2024-11-17 09:06:54 -0300

    add logfile

--- autozap_inf.bat
+++ autozap_inf.bat
@@ -2,6 +2,11 @@
 
 :main
 wait_for_internet
-autozap
+set "logfile=output_%date:~-4,4%%date:~-10,2%%date:~-7,2%_%time:~0,2%%time:~3,2%%time:~6,2%.log"
+set "logfile=%logfile: =0%"
+autozap > %logfile% 2>&1
+if %ERRORLEVEL% EQU 0 (
+  del %logfile%
+)
 timeout /t 600 > NUL
 goto:main
